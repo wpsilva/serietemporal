816d1038e2a65ebf1e6e92cf8ca1c5bc42b13537
wagner <wagnerpdasilva@hotmail.com>
2025-05-07 21:52:18 -0300

    decomposition

--- boigordo.py
+++ boigordo.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
+from dateutil.parser import parse
 
 file_path = 'BoiGordo.csv'
 
@@ -17,3 +22,13 @@ except FileNotFoundError:
     print(f"File '{file_path}' not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
+
+
+def plot_df(df, x, y, title="", xlabel='Date', ylabel='Number of Passengers', dpi=100):
+    plt.figure(figsize=(15,4), dpi=dpi)
+    plt.plot(x, y, color='tab:red')
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    plt.show()
+    
+
+plot_df(df, x=df.index, y=df['LAST'], title='Closed Prices of BoiGordo', xlabel='Date', ylabel='Closed Price')
